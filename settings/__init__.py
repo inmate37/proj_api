66ad677bac6550e36db14dedd2707679733e30eb
inmate37 <x.public.profile@gmail.com>
2022-10-11 15:35:49 +0600

    Fast commit

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,11 +4,10 @@ import os
 # Django
 from django.core.exceptions import ImproperlyConfigured
 
-
-def get_env_variable(env_variable: str) -> str:
-    try:
-        return os.environ[env_variable]
-    except KeyError:
-        raise ImproperlyConfigured(
-            f'Set {env_variable} environment variable'
-        )
+# def get_env_variable(env_variable: str) -> str:
+#     try:
+#         return os.environ[env_variable]
+#     except KeyError:
+#         raise ImproperlyConfigured(
+#             f'Set {env_variable} environment variable'
+#         )
